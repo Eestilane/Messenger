494777dc2700c4079caf6750e4f31c99ff2e08c5
root <root@DESKTOP-KDIQS9D.>
2025-02-10 18:24:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/messenger/RootActivity.kt
@@ -0,0 +1,30 @@
+package com.example.messenger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.messenger.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as
+                NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.chatFragment) {
+                binding.bottomNavigationView.isVisible = false
+            } else {
+                binding.bottomNavigationView.isVisible = true
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/messenger/fragments/SettingsFragment.kt
@@ -0,0 +1,49 @@
+package com.example.messenger.fragments
+
+import android.content.Context.MODE_PRIVATE
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.messenger.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                setDarkTheme()
+            } else {
+                setLightTheme()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setDarkTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        binding.themeText.setText("ТЁМНАЯ ТЕМА")
+    }
+
+    private fun setLightTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding.themeText.setText("СВЕТЛАЯ ТЕМА")
+    }
+}
